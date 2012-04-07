a4cd53d7c454855d88fcd38245dc0c2733aef3ee
Jesus Ramos <jesus-ramos@live.com>
2012-04-06 22:39:39 -0400

    Added FPS counter to spinning cube

--- 3D Cube/main.c
+++ 3D Cube/main.c
@@ -4,9 +4,25 @@
 #include <GL/glut.h>
 #endif /* __APPLE__ */
 
+#include <stdio.h>
+
 int screen_width = 800;
 int screen_height = 600;
 float angle = 0;
+
+unsigned int fps_time;
+unsigned int fps_frames = 0;
+float fps = 0;
+
+void draw_string(float x, float y, void *font, char *string)
+{
+    char *c;
+
+    glColor3f(1.0, 1.0, 1.0);
+    glRasterPos3f(x, y, 0);
+    for (c = string; *c; c++)
+        glutBitmapCharacter(font, *c);
+}
 
 void display()
 {
@@ -55,13 +71,15 @@ void display()
             6, 7, 3
         };
     int i, j;
+    char buf[16];
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0, 1.0f * screen_width / screen_height, 0.1, 10.0);
-    
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(0.0, 2.0, 0.0,
@@ -78,6 +96,14 @@ void display()
             glVertex3fv(&cube_vertices[elements[i * 6 + j] * 3]);
         }
     glEnd();
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    snprintf(buf, 16, "FPS: %.1f", fps);
+    draw_string(-0.9, -0.9, GLUT_BITMAP_HELVETICA_18, buf);
     
     glutSwapBuffers();
 }
@@ -91,8 +117,19 @@ void reshape(int width, int height)
 
 void idle()
 {
+    int d_time;
+    
     angle = glutGet(GLUT_ELAPSED_TIME) / 1000.0 * 45;
 
+    fps_frames++;
+    d_time = glutGet(GLUT_ELAPSED_TIME) - fps_time;
+    if (d_time > 1000.0)
+    {
+        fps = fps_frames / (d_time / 1000.0);
+        fps_time = glutGet(GLUT_ELAPSED_TIME);
+        fps_frames = 0;
+    }
+    
     glutPostRedisplay();
 }
 
@@ -107,10 +144,12 @@ int main(int argc, char **argv)
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glClearColor(0.0, 0.0, 0.0, 0.0);
-    
+
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutIdleFunc(idle);
+
+    fps_time = glutGet(GLUT_ELAPSED_TIME);
 
     glutMainLoop();
 
